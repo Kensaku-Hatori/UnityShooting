8c36bc67f0ef826e9cbb8383d4c5c3dc772f551f
鈴木琉也 <241309@st.yoshida-g.ac.jp>
2025-02-05 10:37:44 +0900

    ボス２

--- Assets/Enemy/EnemyBullet.cs
+++ Assets/Enemy/EnemyBullet.cs
@@ -5,97 +5,29 @@ using UnityEngine;
 public class EnemyBullet : MonoBehaviour
 {
 
-    [SerializeField] float moveSpeed = 3.0f;                   // 移動値
-    [SerializeField] Vector3 moveVec = new Vector3(-1, 0, 0);  // 移動方向
+    float moveSpeed = 3.0f;                   // 移動値
+    Vector3 moveVec = new Vector3(-1, 0, 0);  // 移動方向
 
 
     void Update()
     {
         float add_move = moveSpeed * Time.deltaTime;
         transform.Translate(moveVec * add_move);
+
     }
 
-    //public void SetMoveSpeed(float _speed)
-    //{
-    //    moveSpeed = _speed;
-    //}
+    public void SetMoveSpeed(float _speed)
+    {
+        moveSpeed = _speed;
+    }
 
     public void SetMoveVec(Vector3 _vec)
     {
         moveVec = _vec.normalized;
     }
 
-    enum ShotType
-    {
-        NONE = 0,
-        AIM,            // プレイヤーを狙う
-        THREE_WAY,      // ３方向
-    }
 
-    [System.Serializable]
-    struct ShotData
-    {
-        public int frame;
-        public ShotType type;
-        public EnemyBullet bullet;
-    }
 
-    // ショットデータ
-    [SerializeField] ShotData shotData = new ShotData { frame = 60, type = ShotType.NONE, bullet = null };
 
-    GameObject playerObj = null;    // プレイヤーオブジェクト
-    int shotFrame = 0;              // フレーム
-
-    void Start()
-    {
-        // プレイヤーオブジェクトを取得する
-        switch (shotData.type)
-        {
-            case ShotType.AIM:
-                playerObj = GameObject.Find("player");
-                break;
-        }
-    }
-
-    // ショット処理（これをUpdateなどで呼ぶ）
-    void Shot()
-    {
-        ++shotFrame;
-        if (shotFrame > shotData.frame)
-        {
-            switch (shotData.type)
-            {
-                // プレイヤーを狙う
-                case ShotType.AIM:
-                    {
-                        if (playerObj == null) { break; }
-                        EnemyBullet bullet = (EnemyBullet)Instantiate(
-                            shotData.bullet,
-                            transform.position,
-                            Quaternion.identity
-                        );
-                        bullet.SetMoveVec(playerObj.transform.position - transform.position);
-                    }
-                    break;
-
-                // ３方向
-                case ShotType.THREE_WAY:
-                    {
-                        //弾
-                        EnemyBullet bullet = (EnemyBullet)Instantiate(
-                    shotData.bullet,
-                    transform.position,
-                    Quaternion.identity
-                );
-                        bullet = (EnemyBullet)Instantiate(shotData.bullet, transform.position, Quaternion.identity);
-                        bullet.SetMoveVec(Quaternion.AngleAxis(15, new Vector3(0, 0, 1)) * new Vector3(-1, 0, 0));
-                        bullet = (EnemyBullet)Instantiate(shotData.bullet, transform.position, Quaternion.identity);
-                        bullet.SetMoveVec(Quaternion.AngleAxis(-15, new Vector3(0, 0, 1)) * new Vector3(-1, 0, 0));
-                    }
-                    break;
-            }
-
-            shotFrame = 0;
-        }
-    }
 }
+
---
+++ Assets/Script/BossContrller2.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossContrller2 : MonoBehaviour
+{
+    enum ShotType
+    {
+        NONE = 0,
+        AIM,            // プレイヤーを狙う
+        THREE_WAY,      // ３方向
+    }
+
+    [System.Serializable]
+    struct ShotData
+    {
+        public int frame;
+        public ShotType type;
+        public EnemyBullet bullet;
+    }
+
+    // ショットデータ
+
+    [SerializeField] ShotData shotData = new ShotData { frame = 60, type = ShotType.THREE_WAY, bullet = null };
+
+    GameObject playerObj = null;    // プレイヤーオブジェクト
+    int shotFrame = 0;              // フレーム
+
+    public float speed = -2.0f;
+
+    void Start()
+    {
+
+        // プレイヤーオブジェクトを取得する
+        switch (shotData.type)
+        {
+            case ShotType.AIM:
+                playerObj = GameObject.Find("player");
+                break;
+        }
+    }
+
+    // ショット処理（これをUpdateなどで呼ぶ）
+    void Shot()
+    {
+        ++shotFrame;
+        if (shotFrame > shotData.frame)
+        {
+            switch (shotData.type)
+            {
+                // プレイヤーを狙う
+                case ShotType.AIM:
+                    {
+                        if (playerObj == null) { break; }
+                        EnemyBullet bullet = (EnemyBullet)Instantiate(
+                            shotData.bullet,
+                            transform.position,
+                            Quaternion.identity
+                        );
+                        bullet.SetMoveVec(playerObj.transform.position - transform.position);
+                    }
+                    break;
+
+                // ３方向
+                case ShotType.THREE_WAY:
+                    {
+                        //弾
+                        EnemyBullet bullet = (EnemyBullet)Instantiate(
+                        shotData.bullet,
+                        transform.position,
+                        Quaternion.identity
+                        );
+
+                        bullet = (EnemyBullet)Instantiate(shotData.bullet, transform.position, Quaternion.identity);
+                        bullet.SetMoveVec(Quaternion.AngleAxis(15, new Vector3(0, 0, 1)) * new Vector3(-1, 0, 0));
+                        bullet = (EnemyBullet)Instantiate(shotData.bullet, transform.position, Quaternion.identity);
+                        bullet.SetMoveVec(Quaternion.AngleAxis(-15, new Vector3(0, 0, 1)) * new Vector3(-1, 0, 0));
+                    }
+                    break;
+            }
+
+            shotFrame = 0;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, speed, 0) * Time.deltaTime;
+
+        if (transform.position.y <= -4.5f)
+        {
+            speed = -speed;
+        }
+        if (transform.position.y >= 4.5f)
+        {
+            speed = -speed;
+        }
+        Shot();
+    }
+}
